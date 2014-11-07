d0bcf64ddd9666b45f4a52702ef5cc1cd05d0e34
antonio.pullini <antonio.pullini@7b29e81b-0d0f-40cb-b670-6c1e2575089b>
2014-11-07 08:09:24 +0000

    Added some more apb IPs(cvp_if,gpio,spi_master)
    git-svn-id: https://iis-codendi.ee.ethz.ch/svnroot/PULP@1202 7b29e81b-0d0f-40cb-b670-6c1e2575089b

--- apb_gpio.sv
+++ apb_gpio.sv
@@ -1,10 +1,10 @@
 `define REG_PADDIR      4'b0000 //BASEADDR+0x00
-`define REG_PADIN       4'b0001 //BASEADDR+0x08
-`define REG_PADOUT      4'b0010 //BASEADDR+0x10
-`define REG_INTEN       4'b0011 //BASEADDR+0x18
-`define REG_INTTYPE0    4'b0100 //BASEADDR+0x20
-`define REG_INTTYPE1    4'b0101 //BASEADDR+0x28
-`define REG_INTSTATUS   4'b0110 //BASEADDR+0x30
+`define REG_PADIN       4'b0001 //BASEADDR+0x04
+`define REG_PADOUT      4'b0010 //BASEADDR+0x08
+`define REG_INTEN       4'b0011 //BASEADDR+0x0C
+`define REG_INTTYPE0    4'b0100 //BASEADDR+0x10
+`define REG_INTTYPE1    4'b0101 //BASEADDR+0x14
+`define REG_INTSTATUS   4'b0110 //BASEADDR+0x18
 
 module apb_gpio #(
 		parameter APB_ADDR_WIDTH = 12  //APB slaves are 4KB by default
@@ -31,6 +31,7 @@ module apb_gpio #(
 	logic [31:0] r_gpio_inttype0;
 	logic [31:0] r_gpio_inttype1;
 	logic [31:0] r_gpio_out;
+	logic [31:0] r_gpio_dir;
 	logic [31:0] r_gpio_sync0;
 	logic [31:0] r_gpio_sync1;
 	logic [31:0] r_gpio_in;
@@ -45,7 +46,7 @@ module apb_gpio #(
 	
 	logic  [3:0] s_apb_addr;
 	
-	assign s_apb_addr = PADDR[6:3];
+	assign s_apb_addr = PADDR[5:2];
 	
 	assign s_gpio_rise = r_gpio_sync1 & ~r_gpio_in; //foreach input check if rising edge 
 	assign s_gpio_fall = ~r_gpio_sync1 & r_gpio_in; //foreach input check if falling edge 
@@ -67,7 +68,7 @@ module apb_gpio #(
 		else
 			if (!interrupt && s_rise_int ) //rise interrupt if not already rise
 				interrupt = 1'b1;
-			else if (interrupt && PSEL && PENABLE && !PWRITE && (s_apb_addr == REG_INTSTATUS)) //clears int if status is read
+			else if (interrupt && PSEL && PENABLE && !PWRITE && (s_apb_addr == `REG_INTSTATUS)) //clears int if status is read
 				interrupt = 1'b0;
 	end
 	
@@ -96,15 +97,15 @@ module apb_gpio #(
 			if (PSEL && PENABLE && PWRITE)
 			begin
 				case (s_apb_addr)
-				REG_PADDIR:
+				`REG_PADDIR:
 					r_gpio_dir = PWDATA;
-				REG_PADOUT:	
+				`REG_PADOUT:	
 					r_gpio_out = PWDATA;
-				REG_INTEN:
+				`REG_INTEN:
 					r_gpio_inten = PWDATA;
-				REG_INTTYPE0:
+				`REG_INTTYPE0:
 					r_gpio_inttype0 = PWDATA;
-				REG_INTTYPE1:
+				`REG_INTTYPE1:
 					r_gpio_inttype1 = PWDATA;
 				endcase
 			end
@@ -114,21 +115,25 @@ module apb_gpio #(
 	always_comb
 	begin
 		case (s_apb_addr)
-			REG_PADDIR:
+			`REG_PADDIR:
 				PRDATA = r_gpio_dir;
-			REG_PADIN:	
+			`REG_PADIN:	
 				PRDATA = r_gpio_in;
-			REG_PADOUT:	
+			`REG_PADOUT:	
 				PRDATA = r_gpio_out;
-			REG_INTEN:
+			`REG_INTEN:
 				PRDATA = r_gpio_inten;
-			REG_INTTYPE0:
+			`REG_INTTYPE0:
 				PRDATA = r_gpio_inttype0;
-			REG_INTTYPE1:
+			`REG_INTTYPE1:
 				PRDATA = r_gpio_inttype1;
-			REG_INTSTATUS:
+			`REG_INTSTATUS:
 				PRDATA = {31'h0,interrupt};
 		endcase
 	end
+
+    assign gpio_out = r_gpio_out;
+    assign gpio_dir = r_gpio_dir;
+
 endmodule
 
