269fa1f2604447606d9a6778fa6bae630f144920
Antonio Pullini <antonio.pullini@gmail.com>
2015-04-02 16:28:16 +0200

    Fix to latch in synth

--- apb_gpio.sv
+++ apb_gpio.sv
@@ -5,6 +5,7 @@
 `define REG_INTTYPE0    4'b0100 //BASEADDR+0x10
 `define REG_INTTYPE1    4'b0101 //BASEADDR+0x14
 `define REG_INTSTATUS   4'b0110 //BASEADDR+0x18
+`define REG_POWEREVENT  4'b0111 //BASEADDR+0x1C
 
 `define REG_PADCFG0     4'b1000 //BASEADDR+0x20
 `define REG_PADCFG1     4'b1001 //BASEADDR+0x24
@@ -33,6 +34,7 @@ module apb_gpio #(
 	output logic               [31:0] gpio_out,
 	output logic               [31:0] gpio_dir,
 	output logic      [31:0]    [5:0] gpio_padcfg,
+    output logic                      power_event,
 	output logic                      interrupt
 
 	);
@@ -46,6 +48,7 @@ module apb_gpio #(
 	logic [31:0] r_gpio_sync0;
 	logic [31:0] r_gpio_sync1;
 	logic [31:0] r_gpio_in;
+	logic [31:0] r_powerevent;
 	logic [31:0] s_gpio_rise;
 	logic [31:0] s_gpio_fall;
 	logic [31:0] s_is_int_rise;
@@ -114,6 +117,7 @@ module apb_gpio #(
 			r_gpio_inttype1 =  'h0;
 			r_gpio_out      =  'h0;
 			r_gpio_dir      =  'h0;
+            r_powerevent    =  'h0;
             for (int i=0;i<34;i++)
                 gpio_padcfg[i]  =  'h0;
 		end
@@ -131,6 +135,8 @@ module apb_gpio #(
 					r_gpio_inttype0 = PWDATA;
 				`REG_INTTYPE1:
 				    r_gpio_inttype1 = PWDATA;
+                `REG_POWEREVENT:
+                    r_powerevent    = PWDATA;
                 `REG_PADCFG0:
                 begin
                     gpio_padcfg[0]         = PWDATA[4:0];
@@ -209,6 +215,8 @@ module apb_gpio #(
 				PRDATA = r_gpio_inttype1;
 			`REG_INTSTATUS:
 				PRDATA = r_status;
+            `REG_POWEREVENT:
+                PRDATA = r_powerevent;
             `REG_PADCFG0:
                 PRDATA = {2'b00,gpio_padcfg[3],2'b00,gpio_padcfg[2],2'b00,gpio_padcfg[1],2'b00,gpio_padcfg[0]};
             `REG_PADCFG1:
@@ -225,8 +233,18 @@ module apb_gpio #(
                 PRDATA = {2'b00,gpio_padcfg[27],2'b00,gpio_padcfg[26],2'b00,gpio_padcfg[25],2'b00,gpio_padcfg[24]};
             `REG_PADCFG7:
                 PRDATA = {2'b00,gpio_padcfg[31],2'b00,gpio_padcfg[30],2'b00,gpio_padcfg[29],2'b00,gpio_padcfg[28]};
+            default:
+                PRDATA = 'h0;
 		endcase
 	end
+
+    always_comb
+    begin
+        power_event = 1'b0;
+        for (int e=0;e<32;e++)
+            if (r_powerevent[e] == 1'b1)
+                power_event = gpio_in[e];   
+    end
 
     assign gpio_out = r_gpio_out;
     assign gpio_dir = r_gpio_dir;
