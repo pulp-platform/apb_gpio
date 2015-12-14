dcbac04adbcc9680064e2d95399a879cc6f6d1f1
Andreas Traber <andytraber@gmail.com>
2015-12-14 14:12:53 +0100

    Add license

--- apb_gpio.sv
+++ apb_gpio.sv
@@ -1,3 +1,13 @@
+// Copyright 2015 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the “License”); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 `define REG_PADDIR      4'b0000 //BASEADDR+0x00
 `define REG_PADIN       4'b0001 //BASEADDR+0x04
 `define REG_PADOUT      4'b0010 //BASEADDR+0x08
@@ -124,7 +134,7 @@ module apb_gpio
             r_gpio_out      <=  '0;
             r_gpio_dir      <=  '0;
             r_powerevent    <=  '0;
-            for (int i=0;i<32;i++)  //FIXME ANTONIO CHECK --> i<34 why 34?
+            for (int i=0;i<32;i++)
                 gpio_padcfg[i]  <=  '0;
         end
         else
